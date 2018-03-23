69e6a1c983788d807af856c4de858837c18f0969
Christopher Beards <beardscm@gmail.com>
2018-03-23 12:53:32 -0400

    Compare preHook is Functional

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -39,7 +39,12 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-
+  bcrypt
+    .compare(plainTextPW, this.password)
+    .then(res => {
+      callBack(null, res);   
+    })
+    .catch(err => err);
 };
 
 // if you're really stuck with this at this point, you can reference this document.
